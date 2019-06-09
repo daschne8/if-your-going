9b8e08c6b832275b0b06d733281d20ee48c36a54
daschne8 <daschne8@gmail.com>
2019-06-09 17:43:50 -0400

    login differentiates between admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,11 @@ class SessionsController < ApplicationController
     if occupant && occupant.authenticate(session_params[:password])
       flash[:success] = "Succesfully Logged In"
       session[:occupant_id] = occupant.id
-      redirect_to establishment_occupant_path(occupant.establishment,occupant)
+      if occupant.admin == true
+        redirect_to displays_path
+      else
+        redirect_to establishment_occupant_path(occupant.establishment,occupant)
+      end
     else
       flash[:warning] = "Invalid username/password"
       return redirect_to login_path
