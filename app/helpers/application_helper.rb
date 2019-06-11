456c78bc100e9ada0083e6bc787ade1bc3685d88
daschne8 <daschne8@gmail.com>
2019-06-10 23:16:33 -0400

    basic nav bar added and needed_goods for pathing updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   def current_occupant
     @occupant = (Occupant.find_by(id: session[:occupant_id]) || Occupant.new)
   end
+  
   def logged_in?
     current_occupant.id != nil
   end
